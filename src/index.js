a0cc260fa621f9bc32a404d3d01d7fd88f760440
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-21 12:39:16 -0600

    updates index.js to include dependencies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//dependencies
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+//create httpLink that connects ApolloClient instance with GraphQL API
+//server will run on http://localhost:4000
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+})
+
+//initiate ApolloClient
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+//render root component, app is wrapped in ApolloProvider and client is passed as prop
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
